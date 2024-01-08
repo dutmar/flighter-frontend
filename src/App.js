65ce2df2d1df77f933b35543addfae4f8383371a
Darjan Utmar <darjan.utmar@gmail.com>
2024-01-08 20:16:21 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import Flights from './components/Flights';
+import Login from './components/Login';
 
 function App() {
+  const[loggedIn, setLoggedIn] = React.useState(false);
+
+  const login = () => {
+    setLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavLink to='/flights'>Flights</NavLink>
+      </div>
+      <Routes>
+          <Route path='/flights' render={props => (
+            <Flights {...props} loggedIn={loggedIn} />
+          )} />
+          <Route path='/login' render={props => (
+            <Login {...props} login={login}/>
+          )} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Flights.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+import apiClient from '../services/api';
+ 
+const Flights = () => {
+    const [flights, setFlights] = React.useState([]);
+    React.useEffect(() => {
+        if(props.loggedIn) {
+            apiClient.get('/api/flights')
+                .then(response => {
+                    setFlights(response.data)
+                })
+                .catch(error => console.error(error));
+        }
+    });
+        
+    const flightsList = flights.map((flight) =>
+        <li key={flight.id}>{flight.origin} - {flight.destination}</li>
+    );
+
+    if(props.loggedIn) {
+        return (
+            <ul>{flightsList}</ul>
+        );
+    }
+
+    return (
+        <div>You are not logged in.</div>
+    )
+}
+ 
+export default Flights;
