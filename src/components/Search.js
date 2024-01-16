2c32a7444388d2545b66d784607dffffcf3e721b
Darjan Utmar <darjan.utmar@gmail.com>
2024-01-16 18:25:32 +0100

    Done with search api, works now

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,24 +3,47 @@ import apiClient from "../services/api";
 
 const Search = () => {
   const[origin, setOrigin] = React.useState('');
-  //const[destination, setDestination] = React.useState('');
+  const[destination, setDestination] = React.useState('');
   const [flights, setFlights] = React.useState([]);
-  
-  const handleSubmit = () => {
-    apiClient.get('api/search',{}, {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    apiClient.get('api/search', {
       params: {
-        query: origin
+        origin: origin,
+        destination: destination
       }
     })
     .then(response => {
-      setFlights(response.data.result);
+      setFlights(response.data)
     })
-    .catch(error => console.log(error));
-  };
-
+    .catch(error => console.error(error));
+  }
+  
+  //OVO RADI!!!!!
+  // React.useEffect(() => {
+  //   apiClient.get('api/search?query=${origin}', {
+  //   })
+  //   .then(response => {
+  //     setFlights(response.data)
+  //   })
+  //   .catch(error => console.error(error));
+  // }, []);
+  
   const flightsList = flights.map((flight) =>
-        <li key={flight.id}>{flight.origin} - {flight.destination}</li>
-    );
+        <li key={flight.id}>
+          <div>
+          {flight.origin} - {flight.destination}
+          </div>
+          <div>
+            {flight.airline}
+          </div>
+          <div>
+            {flight.price}$
+          </div>
+          <button>Add to cart</button>
+        </li>
+  );
 
   return (
     <div>
@@ -32,16 +55,20 @@ const Search = () => {
               onChange={e => setOrigin(e.target.value)}
           />
 
-          {/* <input
+          <input
               type="text"
               placeholder="Destination..."
               value={destination}
               onChange={e => setDestination(e.target.value)}
-          /> */}
+          />
         <button type="submit">Search</button>
       </form>
 
-      <ul>{flightsList}</ul>
+      {flightsList.length > 0 ? (
+        <ul>{flightsList}</ul>
+      ) : (
+        <h1>No flights for that route</h1>
+      )}
     </div>
   );
 }
